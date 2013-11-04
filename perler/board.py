3dd642c3fe596baed9bbd683912ceb8023113a5f
Mark McGuire <mark.b.mcg@gmail.com>
2013-11-04 18:51:01 +0000

    kill bad spaces

--- perler/board.py
+++ perler/board.py
@@ -3,4 +3,3 @@ import perler.bead
 def convert_to_perler(pixels, pallette):
     best_match = functools.partial(perler.bead, pallette)
     return [[best_match(rgb) for rgb in row] for row in pixels]
-            
