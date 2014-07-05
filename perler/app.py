86e4b0f36d80e53828ab36cfdc8057f08263912e
Mark McGuire <mark.b.mcg@gmail.com>
2014-07-05 22:57:18 +0000

    Remove print, add alpha

--- perler/app.py
+++ perler/app.py
@@ -7,28 +7,39 @@ def image_to_perler_image(image_path, pallette_path):
     pallette = read_pallette(pallette_path)
     pixels = read_image_pixels(image_path)
     board = perler.board.convert_to_perler(pixels, pallette)
-    print(board)
     perler_image_path = image_path.split('.')[0] + '_perler.' + image_path.split('.')[1]
     make_image(perler_image_path, board)
 
 def make_image(image_path, board):
     x_size, y_size = len(board[0]), len(board)
-    img = Image.new('RGB', (x_size, y_size))
-    pixels = [c.rgb for row in board for c in row]
+    img = Image.new('RGBA', (x_size, y_size))
+    pixels = []
+    for row in board:
+        for c in row:
+            if c:
+               pixel = c.rgb + (255,)
+               pixels.append(pixel)
+            else:
+               pixels.append((0,0,0,0))
     img.putdata(pixels)
     with open(image_path, 'wb') as fp:
         img.save(fp)
 
 def read_image_pixels(image_path):
     img = Image.open(image_path)
-    img.convert('RGB')
+    img.convert('RGBA')
     x_size, y_size = img.size
     pixels = []
     for y in range(y_size):
         row = []
         pixels.append(row)
         for x in range(x_size):
-            row.append(img.getpixel((x,y)))
+            p = img.getpixel((x,y))
+            if p[3] == 0:
+                p = None
+            else:
+                p = p[:3]
+            row.append(p)
     return pixels
 
 def read_pallette(pallette_path):
--- perler/board.py
+++ perler/board.py
@@ -3,4 +3,13 @@ import functools
 
 def convert_to_perler(pixels, pallette):
     best_match = functools.partial(perler.bead.best_match, pallette)
-    return [[best_match(rgb) for rgb in row] for row in pixels]
+    board = []
+    for row in pixels:
+        beads = []
+        board.append(beads)
+        for rgb in row:
+            if rgb:
+                beads.append(best_match(rgb))
+            else:
+                beads.append(None)
+    return board
