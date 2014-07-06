4af06bd4f5737ebe71bb16d6fee67fd34f37f7c7
Mark McGuire <mark.b.mcg@gmail.com>
2014-07-06 13:31:43 -0500

    fix save path, close image

--- perler/app.py
+++ perler/app.py
@@ -1,6 +1,7 @@
 import PIL.Image as Image
 import perler.board
 import perler.bead
+from pathlib import PurePath
 
 
 def image_to_perler_image(image_path, pallette_path):
@@ -16,25 +17,28 @@ def image_to_perler_pdf(image_path, pallette_path):
     pixels = read_image_pixels(image_path)
     board = perler.board.convert_to_perler(pixels, pallette)
     board_pixels = [[c.rgb[:3] if c else None for c in row] for row in board]
-    perler_pdf_path = image_path.split('.')[0] + '_perler.' + 'pdf'
+    perler_pdf_path = PurePath(image_path).stem + '_perler.pdf'
     perler.board.draw_board(board_pixels, perler_pdf_path)
 
 
 def read_image_pixels(image_path):
     img = Image.open(image_path)
-    img = img.convert('RGBA')
-    x_size, y_size = img.size
+    rgb_img = img.convert('RGBA')
+    img.close()
+    x_size, y_size = rgb_img.size
+    pixels = []
     pixels = []
     for y in range(y_size):
         row = []
         pixels.append(row)
         for x in range(x_size):
-            p = img.getpixel((x,y))
+            p = rgb_img.getpixel((x,y))
             if p[3] == 0:
                 p = None
             else:
                 p = p[:3]
             row.append(p)
+    rgb_img.close()
     return pixels
 
 
