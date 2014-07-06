0483882631bfdeb6a7467117a97e5a1942ec274e
Mark McGuire <mark.b.mcg@gmail.com>
2014-07-06 15:49:39 -0500

    add cropping

--- perler/app.py
+++ perler/app.py
@@ -2,6 +2,7 @@ import PIL.Image as Image
 import perler.board
 import perler.bead
 import csv
+import math
 from pathlib import PurePath
 
 
@@ -16,6 +17,7 @@ def image_to_perler_image(image_path, palette_path):
 def image_to_perler_pdf(image_path, palette_path):
     palette = read_palette(palette_path)
     pixels = read_image_pixels(image_path)
+    pixels = crop_transparent(pixels)
     board = perler.board.convert_to_perler(pixels, palette)
     board_pixels = [[c.rgb[:3] if c else None for c in row] for row in board]
     perler_pdf_path = PurePath(image_path).stem + '_perler.pdf'
@@ -24,9 +26,9 @@ def image_to_perler_pdf(image_path, palette_path):
     palette_used = {}
     for row in board:
         for c in row:
-            if c.code in palette_used:
+            if c and c.code in palette_used:
                 palette_used[c.code] = (c, palette_used[c.code][1] + 1)
-            else:
+            elif c:
                 palette_used[c.code] = (c, 0)
     write_palette(palette_used, perler_palette_path)
 
@@ -43,7 +45,7 @@ def read_image_pixels(image_path):
         row = []
         pixels.append(row)
         for x in range(x_size):
-            p = img.getpixel((x,y))
+            p = img.getpixel((x, y))
             if p[3] == 0:
                 p = None
             else:
@@ -51,6 +53,52 @@ def read_image_pixels(image_path):
             row.append(p)
     img.close()
     return pixels
+
+
+def crop_transparent(img_pixels):
+    middle = math.ceil(len(img_pixels[0]) / 2)
+    left_edge, right_edge, top_edge, bottom_edge = len(img_pixels[0]), 0, len(img_pixels), 0
+    found_non_empty_row = False
+    for y, row in enumerate(img_pixels):
+        if any(pixel is not None for pixel in row):
+            # Left
+            max_trans = None
+            for x in range(middle, -1, -1):
+                pixel = row[x]
+                # Maybe contiguous transparency
+                if not pixel and max_trans is None:
+                    max_trans = x
+                # Not contiguous transparency
+                elif pixel and max_trans is not None:
+                    max_trans = None
+                    break
+            if max_trans is not None:
+                left_edge = min(max_trans, left_edge)
+            #Right
+            min_trans = None
+            for x in range(middle, len(img_pixels[0])):
+                pixel = row[x]
+                # Maybe contiguous transparency
+                if not pixel and min_trans is None:
+                    min_trans = x
+                # Not contiguous transparency
+                elif pixel and min_trans is not None:
+                    min_trans = None
+                    break
+            if min_trans is not None:
+                right_edge = max(min_trans, right_edge)
+            found_non_empty_row = True
+            # advance bottom edge
+            if found_non_empty_row:
+                bottom_edge = y
+        # advance top edge
+        elif not found_non_empty_row:
+            top_edge = y
+    if top_edge != len(img_pixels) and bottom_edge != 0:
+        img_pixels = img_pixels[top_edge:bottom_edge+1]
+    if left_edge != len(img_pixels[0]) and right_edge != 0:
+        img_pixels = [row[left_edge:right_edge+1] for row in img_pixels]
+    return img_pixels
 
 
 def read_palette(palette_path):
@@ -61,7 +109,7 @@ def read_palette(palette_path):
                 continue
             code, name, r, g, b, type_, _ = line.split(',')
             palette.append(perler.bead.PerlerColor(code,
-                name, (int(r),int(g),int(b)), type_))
+                                                   name, (int(r), int(g), int(b)), type_))
     return palette
 
 
