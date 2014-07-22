6009e062d802aaf499435d03980f543e1e1a6b79
Mark McGuire <mark.b.mcg@gmail.com>
2014-07-21 23:26:49 -0500

    Refactor edge finding, add transparent conversion first pass

--- perler/img.py
+++ perler/img.py
@@ -1,5 +1,11 @@
 import math
 import PIL.Image as Image
+from enum import Enum
+
+
+class Location(Enum):
+    top_left = 1
+    bottom_right = 2
 
 
 class PerlerImage:
@@ -33,6 +39,47 @@ def read_image(image_path):
     return PerlerImage(pixels, alpha)
 
 
+def find_left_edge(row, rgb=None):
+    middle = math.ceil(len(row) / 2)
+    max_trans = None
+    for x in range(middle, -1, -1):
+        pixel = row[x]
+        # Maybe contiguous transparency
+        if pixel == rgb and max_trans is None:
+            max_trans = x
+        # Not contiguous transparency
+        elif pixel != rgb and max_trans is not None:
+            max_trans = None
+            break
+    return max_trans
+
+
+def find_right_edge(row, rgb=None):
+    rev_edge = find_left_edge(list(reversed(row)), rgb)
+    return len(row) - rev_edge if rev_edge else None
+
+
+def convert_transparent(image, location=Location.top_left):
+    if location == Location.top_left:
+        rgb = image.pixels[0][0]
+        pixels = image.pixels
+    else:
+        rgb = image.pixels[-1][-1]
+        pixels = [[p for p in reversed(row)] for row in reversed(image.pixels)]
+    modified_pixels = []
+    made_transparent = False
+    for row in image:
+        if any(pixel != rgb for pixel in pixels):
+            # Left side
+            left_edge = find_left_edge(row, rgb)
+            # Right side
+            right_edge = find_right_edge(row, rgb)
+            modified_pixels.append([None] * left_edge + row[left_edge:right_edge] + [None] * right_edge)
+        else:
+            modified_pixels.append(row)
+    return PerlerImage(modified_pixels, transparent=made_transparent)
+
+
 def crop_transparent(image):
     img_pixels = image.pixels
     middle = math.ceil(len(img_pixels[0]) / 2)
@@ -41,29 +88,11 @@ def crop_transparent(image):
     for y, row in enumerate(img_pixels):
         if any(pixel is not None for pixel in row):
             # Left
-            max_trans = None
-            for x in range(middle, -1, -1):
-                pixel = row[x]
-                # Maybe contiguous transparency
-                if not pixel and max_trans is None:
-                    max_trans = x
-                # Not contiguous transparency
-                elif pixel and max_trans is not None:
-                    max_trans = None
-                    break
+            max_trans = find_left_edge(row)
             if max_trans is not None:
                 left_edge = min(max_trans, left_edge)
             # Right
-            min_trans = None
-            for x in range(middle, len(img_pixels[0])):
-                pixel = row[x]
-                # Maybe contiguous transparency
-                if not pixel and min_trans is None:
-                    min_trans = x
-                # Not contiguous transparency
-                elif pixel and min_trans is not None:
-                    min_trans = None
-                    break
+            min_trans = find_right_edge(row)
             if min_trans is not None:
                 right_edge = max(min_trans, right_edge)
             found_non_empty_row = True
