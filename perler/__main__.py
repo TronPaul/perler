35d54e39d91455c1a596bc35d9872263f723b417
Mark McGuire <mark.b.mcg@gmail.com>
2014-07-22 21:21:02 -0500

    add no_crop argument

--- perler/__main__.py
+++ perler/__main__.py
@@ -6,8 +6,9 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('image')
     parser.add_argument('palette')
+    parser.add_argument('--no_crop', action='store_false')
     args = parser.parse_args()
-    app.image_to_perler_pdf(args.image, args.palette)
+    app.image_to_perler_pdf(args.image, args.palette, args.no_crop)
 
 if __name__ == '__main__':
     main()
